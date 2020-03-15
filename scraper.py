ca645afdbb1e42f71e050d2ea03dfc36fc2f6ae8
Eric Chiang <ericchiang1121@gmail.com>
2020-03-15 14:31:59 -0400

    Cleaned up code

--- scraper.py
+++ scraper.py
@@ -8,28 +8,28 @@ BASE_URL = 'https://www.basketball-reference.com'
 
 def scrapePlayer(name):
     flName = name.strip().lower().split()
-    # Have to figure out how to check for players with same names (eg. Gary Payton)
+    #TODO check for players with same names (eg. Gary Payton)
     nameUrl = '/'.join([BASE_URL, 'players', flName[1][0], flName[1][:5] + flName[0][:2] + '01.html'])
+    request = requests.get(nameUrl)
     try:
-        request = requests.get(nameUrl)
         if request.status_code != 200:
             raise Exception('Error: Cannot find player')
-        soup = BeautifulSoup(request.content, 'html.parser')
         
-        perGameElem = soup.find('div', id='all_per_game')
-        perGameStats = []
-        for category in perGameElem.find('thead').findAll('th'):
-            perGameStats.append(category.getText())
-        perGameStatRow = perGameElem.find('tbody').findAll('tr')
+        soup = BeautifulSoup(request.content, 'html.parser')
+        statCategories, rowData = [], []
 
-        rowData = []
+        perGameElem = soup.find('div', id='all_per_game')
+        for category in perGameElem.find('thead').findAll('th'):
+            statCategories.append(category.getText())
+
+        perGameStatRow = perGameElem.find('tbody').findAll('tr')
         for row in perGameStatRow:
             temp = []
             for child in row.children:
                 temp.append(child.getText())
             rowData.append(temp)
 
-        data = pd.DataFrame(data=rowData, columns=perGameStats)
+        data = pd.DataFrame(data=rowData, columns=statCategories)
         data.to_csv(path_or_buf=f'./output/NBA_{name}.csv', index=False)
     except Exception as ex:
         print(ex)
@@ -37,27 +37,27 @@ def scrapePlayer(name):
 
 def scrapeSeason(year):
     yearUrl = '/'.join([BASE_URL, 'leagues', f'NBA_{year}_per_game.html'])
+    request = requests.get(yearUrl)
 
     try:
-        request = requests.get(yearUrl)
         if request.status_code != 200:
             raise Exception('Error: Cannot find season')
+        
         soup = BeautifulSoup(request.content, 'html.parser')
+        statCategories, rowData = [], []
 
         dataTableElem = soup.find('div', id='all_per_game_stats')
-        perGameStats = []
         for category in dataTableElem.find('thead').findAll('th'):
-            perGameStats.append(category.getText())
+            statCategories.append(category.getText())
 
         perGameStatRow = dataTableElem.find('tbody').findAll('tr', class_='full_table')
-        rowData = []
         for row in perGameStatRow:
             temp = []
             for child in row.children:
                 temp.append(child.getText())
             rowData.append(temp)
 
-        data = pd.DataFrame(data=rowData, columns=perGameStats)
+        data = pd.DataFrame(data=rowData, columns=statCategories)
         data.to_csv(path_or_buf=f'./output/NBA_{year}_Season.csv', index=False)
     except Exception as ex:
         print(ex)
