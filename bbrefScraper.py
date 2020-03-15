9e8a4871b7d90af5b4d94ee6eaea1893dd9db1b1
Eric Chiang <ericchiang1121@gmail.com>
2020-03-15 13:03:49 -0400

    Added error catching

--- bbrefScraper.py
+++ bbrefScraper.py
@@ -7,11 +7,11 @@ while (flag):
     mode = input('Enter \'1\' to get a player\'s career statistics OR \'2\' to get the data of all players that year: \n')
     if mode == '1':
         player = input('Enter player\'s first and last name in that order (eg. Michael Jordan)\n')
+        scraper.scrapePlayer(player)
         flag = False
     elif mode == '2':
         year = input('Enter season year\n')
+        scraper.scrapeSeason(year)
         flag = False
     else:
         print('Invalid Option\n')
-
-scraper.scrapeSeason(year)
--- scraper.py
+++ scraper.py
@@ -1,57 +1,63 @@
 import requests
-from bs4 import BeautifulSoup, Comment
-import csv
-import constants
+from bs4 import BeautifulSoup
 import pandas as pd
-import pprint
 
 # Basketball Reference's homepage for team data
 BASE_URL = 'https://www.basketball-reference.com'
-pp = pprint.PrettyPrinter(indent=4)
 
 
 def scrapePlayer(name):
     flName = name.strip().lower().split()
     # Have to figure out how to check for players with same names (eg. Gary Payton)
     nameUrl = '/'.join([BASE_URL, 'players', flName[1][0], flName[1][:5] + flName[0][:2] + '01.html'])
-    request = requests.get(nameUrl)
-    soup = BeautifulSoup(request.content, 'html.parser')
+    try:
+        request = requests.get(nameUrl)
+        if request.status_code != 200:
+            raise Exception('Error: Cannot find player')
+        soup = BeautifulSoup(request.content, 'html.parser')
+        
+        perGameElem = soup.find('div', id='all_per_game')
+        perGameStats = []
+        for category in perGameElem.find('thead').findAll('th'):
+            perGameStats.append(category.getText())
+        perGameStatRow = perGameElem.find('tbody').findAll('tr')
 
-    perGameElem = soup.find('div', id='all_per_game')
-    perGameStats = []
-    for category in perGameElem.find('thead').findAll('th'):
-        perGameStats.append(category.getText())
-    perGameStatRow = perGameElem.find('tbody').findAll('tr')
+        rowData = []
+        for row in perGameStatRow:
+            temp = []
+            for child in row.children:
+                temp.append(child.getText())
+            rowData.append(temp)
 
-    rowData = []
-    for row in perGameStatRow:
-        temp = []
-        for child in row.children:
-            temp.append(child.getText())
-        rowData.append(temp)
-
-    data = pd.DataFrame(data=rowData, columns=perGameStats)
-    data.to_excel('output.xlsx')
+        data = pd.DataFrame(data=rowData, columns=perGameStats)
+        data.to_excel('output.xlsx')
+    except Exception as ex:
+        print(ex)
 
 
 def scrapeSeason(year):
     yearUrl = '/'.join([BASE_URL, 'leagues', f'NBA_{year}_per_game.html'])
-    request = requests.get(yearUrl)
-    soup = BeautifulSoup(request.content, 'html.parser')
 
-    dataTableElem = soup.find('div', id='all_per_game_stats')
-    print(dataTableElem)
-    perGameStats = []
-    for category in dataTableElem.find('thead').findAll('th'):
-        perGameStats.append(category.getText())
+    try:
+        request = requests.get(yearUrl)
+        if request.status_code != 200:
+            raise Exception('Error: Cannot find season')
+        soup = BeautifulSoup(request.content, 'html.parser')
 
-    perGameStatRow = dataTableElem.find('tbody').findAll('tr', class_='full_table')
-    rowData = []
-    for row in perGameStatRow:
-        temp = []
-        for child in row.children:
-            temp.append(child.getText())
-        rowData.append(temp)
+        dataTableElem = soup.find('div', id='all_per_game_stats')
+        perGameStats = []
+        for category in dataTableElem.find('thead').findAll('th'):
+            perGameStats.append(category.getText())
 
-    data = pd.DataFrame(data=rowData, columns=perGameStats)
-    data.to_excel('output.xlsx')
+        perGameStatRow = dataTableElem.find('tbody').findAll('tr', class_='full_table')
+        rowData = []
+        for row in perGameStatRow:
+            temp = []
+            for child in row.children:
+                temp.append(child.getText())
+            rowData.append(temp)
+
+        data = pd.DataFrame(data=rowData, columns=perGameStats)
+        data.to_excel('output.xlsx')
+    except Exception as ex:
+        print(ex)
